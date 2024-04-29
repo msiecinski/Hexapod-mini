87643e9f6e60aa6b70ac7e8f34189118cdb11ba2
Michał Sieciński <manrul93@gmail.com>
2024-04-29 11:18:02 +0200

    synch

--- HexapodMiniSourceCode/src/main.cpp
+++ HexapodMiniSourceCode/src/main.cpp
@@ -4,13 +4,23 @@
 
 
 void setup() {
-  Robot &a = Robot::getInstance();
+//  Robot &a = Robot::getInstance();
   // put your setup code here, to run once:
  
      
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
- 
+  Robot &a = Robot::getInstance();
+  
+  static  int counter=0;
+  if(counter >20)
+    counter = 0;
+  else
+  {
+   //a.setAngle(counter,Leg6::joint_2);
+   a.setPosition(-counter,counter,10,Leg6::jo1nts);
+    counter+=5;
+  }
+  delay(500);
 }
